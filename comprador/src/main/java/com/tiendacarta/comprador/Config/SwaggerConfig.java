ace564d975fee43fb1843cdb741a060d8771998a
felipe acuna <acunafelipe272@gmail.com>
2026-06-14 23:34:59 +0000

    Doc: Agregar Swagger

---
+++ comprador/src/main/java/com/tiendacarta/comprador/Config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.tiendacarta.comprador.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Comprador API")
+                        .version("2.0")
+                        .description("API para la gestión de compradores en TiendaCarta"));
+    }
+}
